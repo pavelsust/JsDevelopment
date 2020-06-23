919c4b24566ab700add744645f6d1bfdb0e6dbb1
paveltech <pavelcep.sust11@gmail.com>
2020-06-24 02:59:49 +0600

    some change

--- Js Practise/HelloWorld.js
+++ Js Practise/HelloWorld.js
@@ -4,4 +4,16 @@ var name = 'pavel'
 console.log(name)
 
 name = 1
+
+var a =10
+
+if (a == name){
+    console.log('yes')
+}else {
+    console.log('No')
+}
 console.log(name)
+show()
+function show(){
+    console.log('hello')
+}
