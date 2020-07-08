01dc35761c6c73ed986b2137b9938e1b50cf0028
paveltech <pavelcep.sust11@gmail.com>
2020-07-08 19:53:40 +0600

    class added

--- Js Practise/classes/ClassExample.js
+++ Js Practise/classes/ClassExample.js
@@ -2,30 +2,39 @@
  *  class example
  *
  */
-class Person{
-    constructor(name) {
-    this.name = name
-    }
-    walk(){
-        console.log(this.name)
-    }
-}
+import {Teacher} from "./Teacher";
 
-class Teacher extends Person{
-    constructor(name , age) {
-        super(name)
-        this.age = age
-    }
 
-    showAge(){
-        console.log(this.age)
-    }
-}
-
-let person = new Person('pavel')
-person.walk()
-
-let teacher = new Teacher('robin' ,20)
-teacher.walk()
+let teacher = new Teacher('pavel' , 20)
 teacher.showAge()
+teacher.walk()
 
+
+
+// class Person{
+//     constructor(name) {
+//     this.name = name
+//     }
+//     walk(){
+//         console.log(this.name)
+//     }
+// }
+//
+// class Teacher extends Person{
+//     constructor(name , age) {
+//         super(name)
+//         this.age = age
+//     }
+//
+//     showAge(){
+//         console.log(this.age)
+//     }
+// }
+//
+// let person = new Person('pavel')
+// person.walk()
+//
+// let teacher = new Teacher('robin' ,20)
+// teacher.walk()
+// teacher.showAge()
+//
---
+++ Js Practise/classes/Teacher.js
@@ -0,0 +1,12 @@
+import {Person} from "./Person";
+
+export class Teacher extends Person{
+    constructor(name , age) {
+        super(name)
+        this.age = age
+    }
+
+    showAge(){
+        console.log(this.age)
+    }
+}
